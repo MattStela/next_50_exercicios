2bcde173f1515fab7f5e6280116a119b7588a957
Matheus Alves <matheus.alves.stela@gmail.com>
2024-12-28 19:23:41 -0300

    adicionei uma imagem ao footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="bg-gradient-to-t from-blue-500 to-blue-950 p-4 text-white font-mono">
+    <div className="bg-gradient-to-t from-blue-500 to-blue-950 text-white font-mono">
       <div className="container mx-auto flex flex-row space-x-4 items-center justify-center">
         <a href="https://github.com/MattStela">
           <Image
@@ -13,6 +13,16 @@ export default function Footer() {
             loading="eager"
           />
         </a>
+        <div>
+        <Image 
+            src="/images/espiando.png"
+            alt="espiando.png"
+            width={60}
+            height={25}
+            loading="eager"
+            
+          />
+        </div>
         <a href="https://www.linkedin.com/in/malvetela/">
           <Image
             src="/images/In-Blue-128.png"
--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        
         <main>{children}</main>
         <Footer />
       </body>
